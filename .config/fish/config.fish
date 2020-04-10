a18b44084e59fcce9d727557111b8d9d25cfa6af
Travis Cyronek <cyronek.travis@gmail.com>
2020-04-10 12:23:58 -0700

    modified:   .config/fish/config.fish 	new file:   .config/fish/functions/fish_greeting.fish 	new file:   .config/fish/functions/fish_logo.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,11 @@
 # enable vi-like keybindings, and disable the (ugly) vim-mode prompt
 fish_vi_key_bindings
-#function fish_mode_prompt
-#end
 
 # aliases
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 alias r="/Library/Frameworks/R.framework/Resources/bin/R"
 
 # paths
-set PATH $PATH "/Applications/Julia-1.3.app/Contents/Resources/julia/bin"
 set PATH $PATH "/Applications/Stata/StataSE.app/Contents/MacOS"
 
 # >>> conda initialize >>>
