dbe4ceb1558431c35e3a52e445fdbe1583090adb
Travis Cyronek <29681508+traviscyronek@users.noreply.github.com>
2025-05-28 10:28:53 -0400

    Update .wezterm.lua
    Added a configuration for the tabline

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,9 +1,10 @@
 local wezterm = require('wezterm')
 local config = wezterm.config_builder()
 local canonical_solarized = require('canonical_solarized')
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 
 
-config.default_prog = { 'C:/Users/Travis/scoop/apps/git/current/bin/bash.exe', '-l' }
+config.default_prog = { 'C:/Program Files/Git/bin/bash.exe', '-l' }
 
 
 config.font = wezterm.font('Hack Nerd Font Mono')
@@ -15,7 +16,49 @@ config.color_scheme = 'Canonical Solarized Dark'
 
 
 config.use_fancy_tab_bar = true
-config.hide_tab_bar_if_only_one_tab = true
+config.hide_tab_bar_if_only_one_tab = false
+
+
+tabline.setup({
+  options = {
+    icons_enabled = true,
+    --theme = 'Solarized (dark) (terminal.sexy)',
+    theme = 'Solarized Dark (Gogh)',
+    tabs_enabled = true,
+    theme_overrides = {},
+    section_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+    component_separators = {
+      left = wezterm.nerdfonts.pl_left_soft_divider,
+      right = wezterm.nerdfonts.pl_right_soft_divider,
+    },
+    tab_separators = {
+      left = wezterm.nerdfonts.pl_left_hard_divider,
+      right = wezterm.nerdfonts.pl_right_hard_divider,
+    },
+  },
+  sections = {
+    tabline_a = { 'mode' },
+    tabline_b = { 'workspace' },
+    tabline_c = { ' ' },
+    tab_active = {
+      'index',
+      { 'parent', padding = 0 },
+      '/',
+      { 'cwd', padding = { left = 0, right = 1 } },
+      { 'zoomed', padding = 0 },
+    },
+    tab_inactive = { 'index', { 'process', padding = { left = 0, right = 1 } } },
+    tabline_x = { '' },
+    tabline_y = { 'datetime' },
+    tabline_z = { 'domain' },
+  },
+  extensions = {},
+})
+tabline.apply_to_config(config)
+config.window_decorations = "TITLE | RESIZE"
 
 
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
@@ -36,6 +79,12 @@ config.keys = {
         key = 'q',
         mods = 'LEADER',
         action = wezterm.action.CloseCurrentPane { confirm = true },
+    },
+    -- create a new tab
+    {
+        key = 't',
+        mods = 'LEADER',
+        action = wezterm.action.SpawnTab 'CurrentPaneDomain',
     },
     -- move focus to pane with "leader (hjkl)"
     {
