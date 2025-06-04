1609af4c1b584710b40d242b33a79f413c8534f5
Travis Cyronek <29681508+traviscyronek@users.noreply.github.com>
2025-06-04 07:28:49 -0400

    Update .wezterm.lua
    tab bar at bottom

--- .wezterm.lua
+++ .wezterm.lua
@@ -17,6 +17,7 @@ config.color_scheme = 'Canonical Solarized Dark'
 
 config.use_fancy_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = false
+config.tab_bar_at_bottom = true
 
 
 tabline.setup({
